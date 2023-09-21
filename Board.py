5265bbcabe08f710c53b3c8dde591bf82409b08a
OfirBarOn <ofirb253@gmail.com>
2023-09-21 12:19:58 +0300

    Edit Board

--- Board.py
+++ Board.py
@@ -1,19 +1,28 @@
 class Board:
-    """Defines the boards of the game"""
-
-    def __init__(self):
-        self.board = [[[None] * 3 for i in range(3)] for i in range(3)]
+    def _init_(self):
+        self.board = [[[" "] * 3 for i in range(3)] for i in range(3)]
 
     def display(self):
-        print(self.board)  # - --> ['x', 'o', 'x'][none, 'x', none][....][....][....]
+        for layer in range (3):
+            for row in range(3):
+                print("|", end="")
+                for column in range(3):
+                    print(self.board[layer][row][column] , end="|")
+                print("")
+            print("*******")
+        return
 
     def check_win(self, indexes):
-        ...
+        print("hello")
+        return
 
     def add_move(self, shape):
-        place = input("where do you want to add your move? (add three numbers level ,row ,column): ")
-        separators = " ,"
+        layer = int(input("In which layer do you want to play?: "))
+        place = input("where do you want to add your move? (add two numbers row and column): ")
+        separators = " "
         indexes = place.split(separators)
-        self.board[level][row][column] = shape
+        row = int(indexes[0])
+        column = int(indexes[1])
+        self.board[layer][row][column] = shape
         self.check_win(indexes)
         self.display()
