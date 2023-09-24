8feef1dae5dfa640509a534c07b90b010122f53d
AmneeAk <31921625+AmneeAk@users.noreply.github.com>
2023-09-24 20:23:33 +0300

    new commit Person.py

--- Person.py
+++ Person.py
@@ -1,5 +1,4 @@
 from board import board
-
 class person:
     def __init__(self, number_of_winnings, shape, name):
         self.number_of_winnings = number_of_winnings
@@ -10,9 +9,20 @@ class person:
         return self.name
 
     def playfun(self, board):
-        print(f"{self} it's your turn.")
-        if board.add_move(self.shape):
-            print(f'congratulations {self} you won!')
+        if self.name == 'computer':
+            print("it's the computers turn")
+            board.computerturn(self.shape)
+        else:
+            print(f"{self} it's your turn.")
+            board.add_move(self.shape)
+        board.display()
+        if board.check_win(self.shape):
             self.number_of_winnings += 1
+            if self != 'computer':
+                print(f'congratulations {self} you won!')
+                print(f'you have won {self.number_of_winnings} time(s)!')
+            else:
+                print("I'm sorry You lost")
+                print(f'The computer have won {self.number_of_winnings} time(s)!')
             return True
         return False
