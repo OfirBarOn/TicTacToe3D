2e044bbfe6569efb3485d11916de9c13dbf11062
AmneeAk <31921625+AmneeAk@users.noreply.github.com>
2023-09-24 20:21:50 +0300

    main with computer as player.py

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
+import random
 from board import board
 from person import person
 
 board1 = board()
+
 print("welcome to the 3D tic-tac-toe game")
-name = input("please enter the first player's name: ")
-firstPlayer = person(0, "X", name)
-name = input("please enter the second player's name: ")
-secondPlayer = person(0, "O", name)
-print(f"Hello {firstPlayer.name} and {secondPlayer.name}, let's play the game")
-flag = True
-# currentplayer = random player
-while flag:
-    if not (currentplayer.playfun(board1)):
-        continue
+while True:
+    answer = input("Do you want to play with a computer? (y/n): ")
+    if answer != 'y' and answer != 'n':
+        print("Invalid input, please try again!")
     else:
-        while True:
-            answer = input("Do you want to play another round? (y/n): ")
-            if answer == 'y':
-                flag = True
-                board1 = board()
-                break
-            elif answer == 'n':
-                flag = False
-                break
-            else:
-                print("Invalid input")
+        break
+
+if answer == 'y':
+    name = input("please enter your name: ")
+    firstPlayer = person(0, "X", name)
+    secondPlayer = person(0, "O", 'computer')
+    print(f"Hello {firstPlayer.name}, let's play the game")
+else:
+    name = input("please enter the first player's name: ")
+    firstPlayer = person(0, "X", name)
+    name = input("please enter the second player's name: ")
+    secondPlayer = person(0, "O", name)
+    print(f"Hello {firstPlayer.name} and {secondPlayer.name}, let's play the game")
+
+current_player = random.choice(['X', 'O'])
+if current_player == 'X':
+    First_player_move = True
+else:
+    First_player_move = False
+
+flag = True
+while flag:
+    if First_player_move:
+        First_player_move = False
+        if not firstPlayer.playfun(board1):
+            continue
+
+    else:
+        First_player_move = True
+        if not secondPlayer.playfun(board1):
+            continue
+
+    while True:
+        answer = input("Do you want to play another round? (y/n): ")
+        if answer == 'y':
+            flag = True
+            board1 = board()
+            break
+        elif answer == 'n':
+            flag = False
+            break
+        else:
+            print("Invalid input")
