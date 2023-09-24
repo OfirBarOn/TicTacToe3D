da66a2a732e46cd70f39493a47b35f5cf3014986
AmneeAk <31921625+AmneeAk@users.noreply.github.com>
2023-09-24 20:22:25 +0300

    new functions.py

--- Board.py
+++ Board.py
@@ -1,3 +1,5 @@
+import random
+
 class board:
     def __init__(self):
         self.board = [[[" "] * 3 for i in range(3)] for i in range(3)]
@@ -65,30 +67,53 @@ class board:
         except ValueError:
             return False
 
-    def add_move(self, shape):
-        layer = int(input("In which layer do you want to play?: "))
-        while not self.validate(layer):
-            layer = int(input("Invalid input, please enter a number Between 1, 2, 3: "))
-        layer -= 1
-        place = input("where do you want to add your move?"
-                      "(add two numbers row and column seperated with space): ")
+    def validLayer(self):
         while True:
             try:
+                layer = int(input("In which layer do you want to play?"
+                                  "please enter a number Between 1, 2, 3: "))
+                if self.validate(layer):
+                    return layer
+                else:
+                    print("Invalid Input")
+            except:
+                print("Invalid Input!")
+
+
+    def validColRow(self):
+        while True:
+            try:
+                place = input("where do you want to add your move?"
+                              "(add two numbers row and column seperated with space): ")
                 row, column = map(int, place.split())
                 if self.validate(row) and self.validate(column):
-                    break
+                    place = [row,column]
+                    return place
                 else:
                     print("Invalid Input")
             except (ValueError, IndexError):
                 print("Invalid Input")
-            place = input("where do you want to add your move?"
-                          "(add two numbers row and column seperated with space): ")
 
-        row -= 1
-        column -= 1
-        if (board[layer][row][column] != " ") :
+    def computerturn(self, shape):
+        while True:
+            layer = random.randrange(0, 3)
+            row = random.randrange(0, 3)
+            column = random.randrange(0, 3)
+            if (self.board[layer][row][column] == " "):
+                self.board[layer][row][column] = shape
+                return
+
+
+
+    def add_move(self, shape):
+        layer = self.validLayer()
+        place = self.validColRow()
+        layer -= 1
+        row = place[0] -1
+        column =place[1] - 1
+        print(f'{layer} {row} {column}')
+        if (self.board[layer][row][column] != " ") :
             print("This place is taken, pleace choose another place")
             self.add_move(shape)
         self.board[layer][row][column] = shape
-        self.display()
-        return  self.check_win(shape)
+        return
