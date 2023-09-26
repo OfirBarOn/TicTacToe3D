08363907c45b7393c62229236dac7976baf3d371
AmneeAk <31921625+AmneeAk@users.noreply.github.com>
2023-09-26 19:06:26 +0300

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,4 +1,6 @@
 from board import board
+
+
 class person:
     def __init__(self, number_of_winnings, shape, name):
         self.number_of_winnings = number_of_winnings
@@ -8,13 +10,17 @@ class person:
     def __str__(self):
         return self.name
 
+
     def playfun(self, board):
+        if board.check_full():
+            return True
         if self.name == 'computer':
             print("it's the computers turn")
             board.computerturn(self.shape)
         else:
             print(f"{self} it's your turn.")
             board.add_move(self.shape)
+
         board.display()
         if board.check_win(self.shape):
             self.number_of_winnings += 1
